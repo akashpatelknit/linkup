1693fc03aaf4128a59b8557550f9bb4ffa947cec
akashpatelknit <cotsec14@gmail.com>
2023-12-31 03:49:50 +0530

    cores and access toen

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -1,15 +1,21 @@
+// For CommonJS environment (Node.js):
+const multer = require('multer');
+const path = require('path');
+
+// For ES modules (if using):
 import multer from 'multer';
 import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		const directoryPath = path.join(__dirname, 'public', 'temp');
-		cb(null, directoryPath);
+		// Use fileURLToPath for ES modules or __dirname for CommonJS:
+		const basePath =
+			typeof __dirname !== 'undefined'
+				? __dirname
+				: path.dirname(new URL(import.meta.url).pathname);
+		const uploadPath = path.join(basePath, 'public/temp');
+		cb(null, uploadPath);
+		console.log('file', file);
 	},
 	filename: (req, file, cb) => {
 		cb(null, file.originalname);
