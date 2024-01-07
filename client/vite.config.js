51e67b0cba4bc332d44cdf82b0ba0c53bde14bb3
akashpatelknit <cotsec14@gmail.com>
2024-01-07 17:42:39 +0530

    17:42

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react({ include: '**/*.jsx' })],
+	scripts: {
+		dev: 'vite',
+		build: 'vite build',
+		preview: 'vite preview',
+		host: true,
+	},
 });
